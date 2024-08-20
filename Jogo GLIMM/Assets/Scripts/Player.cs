d2beee3237c2a0e86f4bc79dd41fed7deb628baa
gabriellacerda123 <0000095@sesi-es.org.br>
2024-08-20 12:19:26 -0300

    Personagem Funcionando

--- Jogo GLIMM/Assets/Scripts/Player.cs
+++ Jogo GLIMM/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,13 +8,19 @@ public class Player : MonoBehaviour
 
     public float Speed;
     public float JumpForce;
-    public Rigidbody2D rig;
+
+    public bool isJumping;
+    public bool doubleJump;
+
+    private Rigidbody2D rig;
+    private Animator anim;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();  
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -28,13 +35,62 @@ public class Player : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f);
         transform.position += movement * Time.deltaTime * Speed;
+
+        if(Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        } 
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
     }
+        
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                  
+                    rig.AddForce(new Vector2(0f, JumpForce * 1f), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = true;
+        }
+    }
+
 }
